31e16f5d868db6a9c18405501318cfd59c912449
qiantian zhou <zqtedison@gmail.com>
2020-02-10 14:00:51 -0500

    new

---
+++ auth/auth.js
@@ -0,0 +1,16 @@
+const username = document.getElementById("username");
+
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
+const loginButton = document.getElementById("login-button");
+const loginMessage = document.getElementById("login-message");
+
+//console.log(loginEmail, loginPassword, loginButton);
+
+
+loginButton.onclick = function(){
+    console.log(loginEmail.value, loginPassword.value);
+    loginMessage.textContent = "You logged in.";
+
+
+};
